5c19c533b1d2036a760be1ccbfa1f5929f9b359f
Mohammad Zaki <mohdzaki407@gmail.com>
2026-06-04 00:56:33 +0530

    Switch Windows packaging to EXE and update desktop distribution configs

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -22,10 +22,24 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb, TargetFormat.Exe)
-            packageName = "com.one.task"
+            modules(
+                "java.sql", 
+                "jdk.unsupported", 
+                "java.naming", 
+                "java.management", 
+                "jdk.crypto.ec"
+            )
+            packageName = "OneTask"
             packageVersion = project.findProperty("appVersion")?.toString() ?: "1.0.0"
+            vendor = "OneKore"
+            copyright = "Copyright (c) 2026 OneKore. All rights reserved."
+
             windows {
                 iconFile.set(project.file("metadata/icon.ico"))
+                menu = true // Add to Start Menu
+                shortcut = true // Add to Desktop
+                // Unique GUID for Windows updates/uninstalls
+                upgradeUuid = "a3a63a91-3cfc-4fa1-8d60-0b657639ade5"
             }
             macOS {
                 iconFile.set(project.file("metadata/icon.icns"))
